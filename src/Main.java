7d3141e8e8074c5582b08ca2196c87fe3b026eff
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2022-12-28 16:48:49 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Ways to declare array variable
+
+        //int[] myIntArray = {1,2,3,4,5,6,7,8,9,10};
+        //or
+
+        int[] myIntArray = new int[25];
+
+        //use length; myIntArray.length
+        for (int i=0; i<myIntArray.length; i++){
+            myIntArray[i] = i*10;
+        }
+
+        for(int i=0; i<myIntArray.length; i++){
+            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        }
+
+        System.out.println(myIntArray[0]);
+        System.out.println(myIntArray[6]);
+        System.out.println(myIntArray[8]);
+
     }
 }
